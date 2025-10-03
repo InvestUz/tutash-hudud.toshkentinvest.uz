3597dd7e83be6340aa769ca4501ea3fa5067d895
InvestUz <invest.abdusattorov@gmail.com>
2025-10-03 10:37:03 +0500

    tests

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -24,8 +24,8 @@ class PropertyController extends Controller
     {
         $query = Property::with(['district', 'mahalla', 'street', 'creator']);
 
-        // User permission check
-        if (auth()->user()->role !== 'super_admin' || auth()->user()->role !== 'view_only') {
+        // User permission check - show all properties for super_admin and view_only
+        if (auth()->user()->role !== 'super_admin' && auth()->user()->role !== 'view_only') {
             if (auth()->user()->role === 'district_admin') {
                 $query->where('district_id', auth()->user()->district_id);
             } else {
